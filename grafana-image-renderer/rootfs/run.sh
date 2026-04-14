4f748be9e99b80eac6f5e67448f932ec00d1d369
Jane Jeon <git@janejeon.com>
2026-04-13 20:34:31 -0700

    Pain and suffering

--- grafana-image-renderer/rootfs/run.sh
+++ grafana-image-renderer/rootfs/run.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bashio
 
-export PORT="$(bashio::config 'port')"
-export SERVER_ADDR=":${PORT}"
+export SERVER_ADDR=":8081"
 export AUTH_TOKEN="$(bashio::config 'auth_token')"
 export BROWSER_MAX_WIDTH="$(bashio::config 'browser_max_width')"
 export BROWSER_MAX_HEIGHT="$(bashio::config 'browser_max_height')"
 export BROWSER_FLAGS="$(bashio::config 'browser_flags')"
 export API_SILENCE_REQUEST_LOG_PATH="/healthz"
+export LOG_LEVEL="$(bashio::config 'log_level')"
+
+if bashio::config.has_value 'grafana_service_account_token'; then
+    echo "Setting Grafana service account token"
+    export BROWSER_HEADER="Authorization=Bearer $(bashio::config 'grafana_service_account_token')"
+fi
 
 exec /usr/bin/grafana-image-renderer "$@"
