e058c3618445cf8478ed920edc43cc31163f3104
Jane Jeon <git@janejeon.com>
2026-04-13 23:57:32 -0700

    Are you serious

--- grafana/rootfs/entrypoint.sh
+++ grafana/rootfs/entrypoint.sh
@@ -6,6 +6,7 @@ export GF_SERVER_DOMAIN="$(bashio::config 'domain')"
 
 export GF_RENDERING_SERVER_URL="$(bashio::config 'grafana_rendering_server_url')"
 export GF_RENDERING_CALLBACK_URL="http://$GF_SERVER_DOMAIN:$GF_SERVER_HTTP_PORT/"
+export GF_RENDERING_RENDERER_TOKEN="$(bashio::config 'grafana_rendering_renderer_token')"
 
 export GF_ANALYTICS_ENABLED=false
 export GF_ANALYTICS_REPORTING_ENABLED=false
