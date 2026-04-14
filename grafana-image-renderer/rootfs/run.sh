216255ad108fefe37b546ac34252bd7d0475a1b6
Jane Jeon <git@janejeon.com>
2026-04-13 17:30:15 -0700

    v2

--- grafana-image-renderer/rootfs/run.sh
+++ grafana-image-renderer/rootfs/run.sh
@@ -1,15 +1,11 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/usr/bin/env bashio
 
-OPTIONS=/data/options.json
-
-# Read HA addon configuration
-export SERVER_AUTH_TOKEN=$(jq -r '.auth_token' "$OPTIONS")
-export PORT=$(jq -r '.port // 8081' "$OPTIONS")
+export PORT="$(bashio::config 'port')"
 export SERVER_ADDR=":${PORT}"
-export BROWSER_MAX_WIDTH=$(jq -r '.browser_max_width // 1920' "$OPTIONS")
-export BROWSER_MAX_HEIGHT=$(jq -r '.browser_max_height // 1080' "$OPTIONS")
-export BROWSER_FLAG=$(jq -r '.browser_flags // "--disable-dev-shm-usage,--no-sandbox,--disable-extensions"' "$OPTIONS")
+export SERVER_AUTH_TOKEN="$(bashio::config 'auth_token')"
+export BROWSER_MAX_WIDTH="$(bashio::config 'browser_max_width')"
+export BROWSER_MAX_HEIGHT="$(bashio::config 'browser_max_height')"
+export BROWSER_FLAGS="$(bashio::config 'browser_flags')"
 export API_SILENCE_REQUEST_LOG_PATH="/healthz"
 
-exec tini -- /usr/bin/grafana-image-renderer server
+exec /usr/bin/grafana-image-renderer "$@"
