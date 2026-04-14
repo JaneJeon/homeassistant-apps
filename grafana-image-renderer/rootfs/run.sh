731cd9247892c5497dd93da21f40102841173fac
Jane Jeon <git@janejeon.com>
2026-04-13 22:19:20 -0700

    idk what else I can try

--- grafana-image-renderer/rootfs/run.sh
+++ grafana-image-renderer/rootfs/run.sh
@@ -8,6 +8,13 @@ export BROWSER_FLAGS="$(bashio::config 'browser_flags')"
 export API_SILENCE_REQUEST_LOG_PATH="/healthz"
 export LOG_LEVEL="$(bashio::config 'log_level')"
 
+# Desperation...
+export BROWSER_READINESS_DOM_HASHCODE_TIMEOUT=1s
+export BROWSER_READINESS_GIVE_UP_ON_ALL_QUERIES=1s
+export BROWSER_READINESS_ITERATION_INTERVAL=5s
+export BROWSER_READINESS_NETWORK_IDLE_TIMEOUT=1s
+export BROWSER_READINESS_PRIOR_WAIT=5s
+
 if bashio::config.has_value 'grafana_service_account_token'; then
     echo "Setting Grafana service account token"
     export BROWSER_HEADER="Authorization=Bearer $(bashio::config 'grafana_service_account_token')"
