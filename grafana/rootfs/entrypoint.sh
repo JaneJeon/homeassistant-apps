a239f9ef4df521c97cf47f4b27fdf75a7361ff3a
Jane Jeon <git@janejeon.com>
2026-04-15 12:17:32 -0700

    Enable profiling

--- grafana/rootfs/entrypoint.sh
+++ grafana/rootfs/entrypoint.sh
@@ -6,6 +6,12 @@ export GF_PATHS_DATA=$HA_DATA_DIR/data
 export GF_PATHS_LOGS=$HA_DATA_DIR/logs
 export GF_PATHS_PLUGINS=$HA_DATA_DIR/plugins
 
+if bashio::config 'enable_profiling'; then
+    export GF_DIAGNOSTICS_PROFILING_ENABLED=true
+    export GF_DIAGNOSTICS_PROFILING_ADDR=0.0.0.0
+    export GF_DIAGNOSTICS_PROFILING_PORT=8079
+fi
+
 export GF_SERVER_HTTP_PORT=8080
 export GF_SERVER_DOMAIN="$(bashio::config 'domain')"
 
